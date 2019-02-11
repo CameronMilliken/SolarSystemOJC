069c37140fd6c07851e032add7a40687476d2e59
Cameron <cameron.milliken@gmail.com>
2019-02-11 10:06:02 -0700

    Initial commit

---
+++ SolarSystem-C24/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SolarSystem-C24
+//
+//  Created by Cameron Milliken on 2/11/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
